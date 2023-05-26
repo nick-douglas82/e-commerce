cd61973111cff2f43c31c7250d671b38cdabffa1
Nick Douglas <nick.douglas82@gmail.com>
2023-05-26 10:16:53 +0100

    Adding noindex

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
+
+useHead({
+    meta: [
+        {
+            name: 'robots',
+            content: 'noindex, nofollow',
+        },
+        {
+            name: 'x-robots-tag',
+            content: 'noindex, nofollow',
+        },
+    ],
+})
 </script>
 
 <template>
