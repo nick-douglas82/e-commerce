ac1130558bf980c6edc0f11ef18ae68141b86124
Nick Douglas <nick.douglas82@gmail.com>
2023-06-19 14:43:49 +0100

    Clean up!

--- pages/products/[productid].vue
+++ pages/products/[productid].vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { StarIcon } from '@heroicons/vue/20/solid'
-import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
-import { Product, ProductWithCategories } from '~/types/Products'
+import { ProductWithCategories } from '~/types/Products'
 import {
     CurrencyDollarIcon,
     GlobeAmericasIcon,
@@ -24,56 +21,6 @@ const addToBasket = (product: ProductWithCategories) => {
     basketStore.addItemsToBasket(product)
 }
 
-// const product = {
-//     name: 'Basic Tee',
-//     price: '$35',
-//     rating: 3.9,
-//     reviewCount: 512,
-//     href: '#',
-//     breadcrumbs: [
-//         { id: 1, name: 'Women', href: '#' },
-//         { id: 2, name: 'Clothing', href: '#' },
-//     ],
-//     images: [
-//         {
-//             id: 1,
-//             imageSrc:
-//                 'https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg',
-//             imageAlt: "Back of women's Basic Tee in black.",
-//             primary: true,
-//         },
-//     ],
-//     colors: [
-//         {
-//             name: 'Black',
-//             bgColor: 'bg-gray-900',
-//             selectedColor: 'ring-gray-900',
-//         },
-//         {
-//             name: 'Heather Grey',
-//             bgColor: 'bg-gray-400',
-//             selectedColor: 'ring-gray-400',
-//         },
-//     ],
-//     sizes: [
-//         { name: 'XXS', inStock: true },
-//         { name: 'XS', inStock: true },
-//         { name: 'S', inStock: true },
-//         { name: 'M', inStock: true },
-//         { name: 'L', inStock: true },
-//         { name: 'XL', inStock: false },
-//     ],
-//     description: `
-//       <p>The Basic tee is an honest new take on a classic. The tee uses super soft, pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally, with a special dye technique that gives each tee it's own look.</p>
-//       <p>Looking to stock your closet? The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.</p>
-//     `,
-//     details: [
-//         'Only the best materials',
-//         'Ethically and locally made',
-//         'Pre-washed and pre-shrunk',
-//         'Machine wash cold with similar colors',
-//     ],
-// }
 const policies = [
     {
         name: 'International delivery',
@@ -86,9 +33,6 @@ const policies = [
         description: "Don't look at other tees",
     },
 ]
-
-// const selectedColor = ref(product.colors[0])
-// const selectedSize = ref(product.sizes[2])
 </script>
 
 <template>
@@ -117,43 +61,6 @@ const policies = [
                             :product="product"
                             :variants="product.variants"
                         />
-                        <!-- Reviews -->
-                        <div class="mt-4">
-                            <h2 class="sr-only">Reviews</h2>
-                            <div class="flex items-center">
-                                <!-- <p class="text-sm text-gray-700">
-                                    {{ product.rating }}
-                                    <span class="sr-only"> out of 5 stars</span>
-                                </p> -->
-                                <!-- <div class="flex items-center ml-1">
-                                    <StarIcon
-                                        v-for="rating in [0, 1, 2, 3, 4]"
-                                        :key="rating"
-                                        :class="[
-                                            product.rating > rating
-                                                ? 'text-yellow-400'
-                                                : 'text-gray-200',
-                                            'h-5 w-5 flex-shrink-0',
-                                        ]"
-                                        aria-hidden="true"
-                                    />
-                                </div> -->
-                                <!-- <div
-                                    aria-hidden="true"
-                                    class="ml-4 text-sm text-gray-300"
-                                >
-                                    ·
-                                </div>
-                                <div class="flex ml-4">
-                                    <a
-                                        href="#"
-                                        class="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                                        >See all
-                                        {{ product.reviewCount }} reviews</a
-                                    >
-                                </div> -->
-                            </div>
-                        </div>
                     </div>
 
                     <!-- Image gallery -->
@@ -190,23 +97,6 @@ const policies = [
                                 class="mt-4 prose-sm prose text-gray-500"
                                 v-html="product.description"
                             />
-                        </div>
-
-                        <div class="pt-8 mt-8 border-t border-gray-200">
-                            <!-- <h2 class="text-sm font-medium text-gray-900">
-                                Fabric &amp; Care
-                            </h2> -->
-
-                            <!-- <div class="mt-4 prose-sm prose text-gray-500">
-                                <ul role="list">
-                                    <li
-                                        v-for="item in product.details"
-                                        :key="item"
-                                    >
-                                        {{ item }}
-                                    </li>
-                                </ul>
-                            </div> -->
                         </div>
 
                         <!-- Policies -->
